3c47489edb2dbdcc06a10c8e56a9a660ad23b7f2
Philip Crowther <crowtherpb@gmail.com>
2022-10-26 17:51:13 -0400

    feat: update render function to print more legibly

--- gameoflife.py
+++ gameoflife.py
@@ -7,12 +7,10 @@ class GameBoard:
         self.board_state = [[randint(0,1) for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
     def render(self):
-        for row in range(len(self.board_state)):
-            print(self.board_state[row])
+        for row in range(BOARD_SIZE):
+            print(' '.join([' ' if self.board_state[row][col] == 0 else '#' for col in range(BOARD_SIZE)]))
 
 
 if __name__ == "__main__":
     game = GameBoard()
-    game.render()
-
-    
+    game.render()    
