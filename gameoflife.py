4210f21c09db4238fba4b041b0c1934c4de24549
Philip Crowther <crowtherpb@gmail.com>
2022-10-26 18:01:46 -0400

    feat: update render function with borders to increase legibility

--- gameoflife.py
+++ gameoflife.py
@@ -7,8 +7,10 @@ class GameBoard:
         self.board_state = [[randint(0,1) for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
     def render(self):
+        print('', ''.join(['—' for _ in range((BOARD_SIZE * 2) + 1)]))
         for row in range(BOARD_SIZE):
-            print(' '.join([' ' if self.board_state[row][col] == 0 else '#' for col in range(BOARD_SIZE)]))
+            print('|', ' '.join([' ' if self.board_state[row][col] == 0 else '#' for col in range(BOARD_SIZE)]), '|')
+        print('', ''.join(['—' for _ in range((BOARD_SIZE * 2) + 1)]))
 
 
 if __name__ == "__main__":
