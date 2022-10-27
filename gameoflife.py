645d668d50aba17c3ea14d0d45a4175611989c13
Philip Crowther <crowtherpb@gmail.com>
2022-10-27 16:02:49 -0400

    feat: complete function that calculates next state

--- gameoflife.py
+++ gameoflife.py
@@ -1,18 +1,72 @@
 from random import randint
+import time
 
-BOARD_SIZE = 10
+BOARD_SIZE = 20
 
 class GameBoard:
+    """Conway's Game of Life class"""
     def __init__(self):
         self.board_state = [[randint(0,1) for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
     def render(self):
+        # print top border
         print('', ''.join(['—' for _ in range((BOARD_SIZE * 2) + 1)]))
+        # print content
         for row in range(BOARD_SIZE):
             print('|', ' '.join([' ' if self.board_state[row][col] == 0 else '#' for col in range(BOARD_SIZE)]), '|')
+        # print bottom border
         print('', ''.join(['—' for _ in range((BOARD_SIZE * 2) + 1)]))
+
+    def next_state(self):
+        # calculate next board state
+        def count_neighbors(row, col):
+            # count number of neighbors
+            row_start = row
+            row_stop = row
+            col_start = col
+            col_stop = col
+
+            if row > 0:
+                row_start -= 1
+            if row < BOARD_SIZE - 1:
+                row_stop += 1
+            if col > 0:
+                col_start -= 1
+            if col < BOARD_SIZE - 1:
+                col_stop += 1
+            
+            count = 0
+            for r in range(row_start, row_stop):
+                for c in range(col_start, col_stop):
+                    if r == row and c == col:
+                        continue
+                    count += self.board_state[r][c]
+            return count
+
+        number_of_neighbors = [[count_neighbors(row, col) for col in range (BOARD_SIZE)] for row in range(BOARD_SIZE)]
+
+        # print(number_of_neighbors)
+
+        for row in range(BOARD_SIZE):
+            for col in range(BOARD_SIZE):
+                # iterate through each position
+                if number_of_neighbors[row][col] == 3:
+                    # every position with three neighbors comes back
+                    self.board_state[row][col] = 1
+                elif self.board_state[row][col] == 0:
+                    # position is dead
+                    continue
+                elif number_of_neighbors[row][col] == 2:
+                    # position is alive
+                    self.board_state[row][col] = 1
+                else: # position is dead
+                    self.board_state[row][col] = 0
+
 
 
 if __name__ == "__main__":
     game = GameBoard()
-    game.render()    
+    for _ in range(10):
+        game.render()    
+        game.next_state()
+        time.sleep(.5)
